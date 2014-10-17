7249d4d113fd1be12c319d26f9790e1056f5557d
Matthew Andrews <matt@mattandre.ws>
2014-10-17 10:01:03 +0100

    Start to give life to My FT

--- build/main.js
+++ build/main.js
@@ -1102,4 +1102,40 @@ reqwest('http://next-companies-et-al.herokuapp.com/v1/ubernav.json', function(re
 		+ '</ul>';
 });
 
+// TODO: Make this with real data
+var stuffTypes = {
+	favourites: 'Favourites',
+	forlaters: 'Saved For Later'
+};
+var stuff = {
+	favourites: [{
+		"type": "favourite",
+		"resourceType": "stream",
+		"created": "1413364682107",
+		"uuidv3": "sections:China",
+		"displayText": "China",
+		"id": "activity:1e7983cd0afa3cbf01e463d2c883f3e1a718cf13"
+	}],
+	forlaters: [{
+		"type": "forlater",
+		"resourceType": "article",
+		"created": "1413535332073",
+		"uuidv3": "71244458-55c9-11e4-a3c9-00144feab7de",
+		"displayText": "BoE’s Haldane favours a delay in interest rate rises",
+		"id": "activity:fc7c53b8c939e9cb117ad2c90750a9bc3846728a"
+	}]
+};
+header.querySelector('.o-header__secondary--myft-js').innerHTML = '<ul class="uber-index">'
+	+ Object.keys(stuff).map(function(key) {
+		return '<li class="uber-index__title" data-o-grid-colspan="6 M6 L6 XL6">'
+			+ stuffTypes[key]
+			+ '<ul class="uber-index__children">'
+			+ stuff[key].map(function(child) {
+				return '<li class="uber-index__child"><a href="' + child.uuidv3 + '">' + child.displayText + '</a></li>';
+			}).join('')
+			+ '</ul>'
+			+ '</li>';
+		}).join('');
+		+ '</ul>';
+
 },{"./bower_components/dom-delegate/lib/delegate.js":2,"reqwest":1}]},{},[3])
